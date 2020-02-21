879a56d5f5fd7f963ac98e3d09d318954b40232c
Kyle Talmage <ktalmage85@gmail.com>
2020-02-21 23:46:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,7 +90,6 @@ end
       elsif (@board[win_combo[0]]) == "O" && (@board[win_combo][1]]) == "O" && (@board[win_combo][2]]) == "O"
         return win_combo
       end
-        return false
     end
   end
       
