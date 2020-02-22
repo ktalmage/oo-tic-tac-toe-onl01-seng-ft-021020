b8ebf373211c1d4f4a093e0fd949e1ccc574ebed
Kyle Talmage <ktalmage85@gmail.com>
2020-02-22 00:40:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,7 +85,7 @@ end
 
   def won?
     WIN_COMBINATIONS.detect do |win_combo|
-      binding.pry
+    
       if(@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
         return win_combo
       elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
